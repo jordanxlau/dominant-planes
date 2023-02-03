a5c3a8c4926122750ac6455080912d250998f4f9
jordanxlau <jordanxlau@yahoo.ca>
2023-02-02 20:51:52 -0500

    finished PointCloud file - no testing

--- PointCloud.java
+++ PointCloud.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-import java.lang.Math;
+import java.lang.*;
 
 public class PointCloud implements Iterator{
 
@@ -12,9 +12,9 @@ public class PointCloud implements Iterator{
 		this.filename = filename;
 		cloud = new LinkedList<Point3D>();
 
-		Iterator i = iterator(filename);
+		Iterator i = this.iterator();
 		while (i.hasNext()){
-			cloud.add(i.next());
+			cloud.add((Point3D) i.next());
 		}
 	}
 
@@ -26,29 +26,35 @@ public class PointCloud implements Iterator{
 	// ITERATOR
 	// Iterator Instance variables
     protected Point3D current;
+    protected int currentLine;
+    protected BufferedReader br;
 
     //An iterator method that returns an iterator to the points in the cloud
     public Iterator iterator() {
         current = null;
-        return this.Iterator;
+        currentLine = 1;
+        try{
+			br = new BufferedReader(new FileReader(filename));
+		} catch (IOException e){}
+        return this;
     }
 
-    public Object next() {
-        try{
-            BufferedReader br = new BufferedReader(new FileReader(filename));
-            String[] array = br.readLine().split(","); // read row and process point information
-            current = new Point3D(Double.valueOf(array[0]), Double.valueOf(array[1]), Double.valueOf(array[2]));
-            br.close();
-        } catch (IOException e) {}
-        return current;
+    public Point3D next() {
+		try{
+			String[] array = br.readLine().split(" "); // read row and process point information
+			current = new Point3D(Double.valueOf(array[0]), Double.valueOf(array[1]), Double.valueOf(array[2]));
+		} catch (IOException e){}
+		
+		currentLine++;
+		return current;
     }
 
     public boolean hasNext(){
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(filename));
-            br.close();
+    	try{
             return br.readLine() != null;
-        } catch (IOException e) {}
+        } catch (IOException e) {
+        	return false;
+        }
     }
 
     //END OF ITERATOR
@@ -64,13 +70,16 @@ public class PointCloud implements Iterator{
 	
 	//A getPoint method that returns a random point from the cloud
 	public Point3D getPoint(){
-		int max = cloud.size() - 1;//max is exclusive
-		Math.floor(Math.random() *(max - 0 + 1));
+		int size = cloud.size();
+		int index = (int) Math.floor(Math.random() * size);
+		return cloud.get(index);
 	}
 	
 	//A save method that saves the point cloud into a xyz file
 	public void save(String filename){
-
-	}
+        for (int i = 0; i < cloud.size(); i++){
+        	addPoint(cloud.get(i));
+        }
+    }
 
 }
